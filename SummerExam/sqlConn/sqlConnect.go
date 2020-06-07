e77f1567aef416f5123bc8194fdfb1ca24d93d89
Hoshino Syou(love Katou Megumi) <61332493+ZXHoshinoSyou@users.noreply.github.com>
2020-06-07 17:33:50 +0800

    Update sqlConnect.go

--- SummerExam/sqlConn/sqlConnect.go
+++ SummerExam/sqlConn/sqlConnect.go
@@ -9,7 +9,7 @@ import (
 var db *gorm.DB
 
 func Init() *gorm.DB {
-	open, err := gorm.Open("mysql", "root:syouZX@(127.0.0.1:3306)/bilibiliproject?charset=utf8")
+	open, err := gorm.Open("mysql", "root:@(127.0.0.1:3306)/wuzichess?charset=utf8")
 	if err != nil {
 		log.Fatal("连接数据库失败喵！错误信息：", err)
 		return db
